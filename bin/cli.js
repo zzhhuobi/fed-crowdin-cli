5d18612207c2bee46a44e67f8f60d341ba6de06a
zhaozhenhua <zhaozhenhua@huobi.com>
2021-06-08 17:09:39 +0800

    feat: init

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,20 @@
 let program = require('commander');
 const pkg = require('../package.json');
 const chalk = require('chalk');
+const Crowdin = require('../lib/crowdin');
 
 program
+    .usage('fed-crowdin-cli')
     .version(pkg.version, '-v, --version')
-    .description(chalk(`[ ${pkg.description} - ${pkg.version.toUpperCase()} ]`).green);
+    .description(chalk(`[ ${pkg.description} - ${pkg.version} ]`).green);
 
 program
-    .command('rm <dest> [otherDirs...]')
-    .alias('r')
-    .option('-r, --recursive', 'Remove recursively')
-    .option('-d --drink [drink]', 'Drink', 'Beer')
-    .action(function (d, otherD, cmd) {
-        console.log('remove ' + d, (cmd.drink), (cmd.recursive));
-        if (otherD) {
-            otherD.forEach(function (oDir) {
-                console.log('rmdir %s', oDir);
-            });
-        }
-
+    .command('pull [lang]')
+    .description('Pull translations. When there are no parameters, pull all translations.')
+    .action(function (args,otherArgs,cmd) {
+        const crowdin = new Crowdin();
+        console.log(cmd.args);
+        crowdin.pull(cmd.args);
     });
 
 program.parse(process.argv);
